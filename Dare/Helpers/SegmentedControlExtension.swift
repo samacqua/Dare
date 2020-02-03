5cd8d3a27a058d8a39d50f129b66ce8407dbfccf
Sam Acquaviva <samacqua@gmail.com>
2020-02-02 23:04:47 -0500

    The choose dare and saved dare UIs are better, but still no functionality. Beginning to add that w/ the Category VC

---
+++ Dare/Helpers/SegmentedControlExtension.swift
@@ -0,0 +1,60 @@
+//
+//  SegmentedControlExtension.swift
+//  Dare
+//
+//  Created by Sam Acquaviva on 2/2/20.
+//  Copyright © 2020 Sam Acquaviva. All rights reserved.
+//
+
+import UIKit
+
+extension UISegmentedControl{
+    func removeBorder(){
+        let backgroundImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: self.bounds.size)
+        self.setBackgroundImage(backgroundImage, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .highlighted, barMetrics: .default)
+
+        let deviderImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: CGSize(width: 1.0, height: self.bounds.size.height))
+        self.setDividerImage(deviderImage, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+        self.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.gray, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)], for: .normal)
+        self.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.orange, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)], for: .selected)
+    }
+
+    func addUnderlineForSelectedSegment(){
+        removeBorder()
+        let underlineWidth: CGFloat = self.bounds.size.width / CGFloat(self.numberOfSegments)
+        let underlineHeight: CGFloat = 1.0
+        let underlineXPosition = CGFloat(selectedSegmentIndex * Int(underlineWidth))
+        let underLineYPosition = self.bounds.size.height - 1.0
+        let underlineFrame = CGRect(x: underlineXPosition, y: underLineYPosition, width: underlineWidth, height: underlineHeight)
+        let underline = UIView(frame: underlineFrame)
+        underline.backgroundColor = UIColor.orange
+        underline.tag = 1
+        underline.layer.zPosition = 1
+        self.addSubview(underline)
+    }
+
+    func changeUnderlinePosition(){
+        guard let underline = self.viewWithTag(1) else {return}
+        underline.layer.zPosition = 1
+        let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
+        UIView.animate(withDuration: 0.2, animations: {
+            underline.frame.origin.x = underlineFinalXPosition
+        })
+    }
+}
+
+extension UIImage{
+
+    class func getColoredRectImageWith(color: CGColor, andSize size: CGSize) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let graphicsContext = UIGraphicsGetCurrentContext()
+        graphicsContext?.setFillColor(color)
+        let rectangle = CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height)
+        graphicsContext?.fill(rectangle)
+        let rectangleImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return rectangleImage!
+    }
+}
