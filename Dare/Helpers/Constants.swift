e6c9f0712b948c42145cd3fb1a377398201af8a4
Sam Acquaviva <samacqua@gmail.com>
2020-02-06 12:17:00 -0500

    Began to clean up code. Now selecting post on profile page brings you to same post within larger collectionNode

--- Dare/Helpers/Constants.swift
+++ Dare/Helpers/Constants.swift
@@ -10,19 +10,6 @@ import Foundation
 
 struct Constants {
     
-    struct Authentication {
-        static let signUpEmailToPasswordSegue = "SignUpEmailToPasswordSegue"
-        static let signUpEmailToAlreadySignedUpPassword = "SegueToEmailPasswordLogin"
-        static let preRegPasswordToMain = "PreRegPasswordToMain"
-        static let emailSignUpToMain = "EmailSignUpToMain"
-        static let emailSignInToMain = "EmailSignInToMain"
-        static let signUpToMain = "SignUpToMain"
-        static let logInToMain = "LogInToMain"
-    }
-    
-    struct Main {
-        static let cameraToMain = "CameraToPostSegue"
-        static let darePostToHome = "DarePostToHome"
-    }
+    static let placeholderAssetURL = URL(string: "http://www.w3schools.com/html/mov_bbb.mp4")!
     
 }
