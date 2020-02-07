f1447fed9307375202162ba27015c8224c1bf0ef
Sam Acquaviva <samacqua@gmail.com>
2020-02-07 00:19:24 -0500

    Organized code and added launch screen

--- Dare/Helpers/Extensions.swift
+++ Dare/Helpers/Extensions.swift
@@ -58,3 +58,15 @@ extension UIImage{
         return rectangleImage!
     }
 }
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
