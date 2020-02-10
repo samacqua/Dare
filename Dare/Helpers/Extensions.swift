bbb413e9f9e4352025b83590b1ff4e48916397a3
Sam Acquaviva <samacqua@gmail.com>
2020-02-10 12:49:11 -0500

    Made tab bar clear during home screen, and fixed issue with going onto following/followers of others' profiles.

--- Dare/Helpers/Extensions.swift
+++ Dare/Helpers/Extensions.swift
@@ -113,3 +113,16 @@ extension Date {
         return "\(Int(secondsAgo / year)) years ago"
     }
 }
+
+extension UIImage {
+    static func from(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
